e00bf4982f754a02a959bed0e85463b8e8ae758f
guo_x <guo_x0315@163.com>
2024-01-07 17:22:57 +0800

    该版本修改了网络结构，添加了LSTM网络，LSTM网络在前，TextCNN网络在后。

--- classifier/nn/model.py
+++ classifier/nn/model.py
@@ -14,7 +14,7 @@ class Block(nn.Module):
     def __init__(self, kernel_size, out_channels=config['hidden_layer']):
         super(Block, self).__init__()
         self.sequential = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=out_channels, kernel_size=(kernel_size, config['embedding_dim'])),
+            nn.Conv2d(in_channels=1, out_channels=out_channels, kernel_size=(kernel_size, config['lstm_hidden_layer'])),
             nn.ReLU()
         )
         self.maxpool = nn.MaxPool1d(kernel_size=(config['max_length'] - kernel_size + 1))
@@ -25,19 +25,29 @@ class Block(nn.Module):
         x = x.squeeze(-1)   # 删掉最后一个维度
         x_maxpool = self.maxpool(x)
         x_maxpool = x_maxpool.squeeze(-1)  # 删掉最后一个维度
-        x_avepool = self.avepool(x)
-        x_avepool = x_avepool.squeeze(-1)
-        concat_x = torch.cat((x_maxpool, x_avepool), dim=1)
-        return concat_x
+        # x_avepool = self.avepool(x)
+        # x_avepool = x_avepool.squeeze(-1)
+        # concat_x = torch.cat((x_maxpool, x_avepool), dim=1)
+        # return concat_x
+        return x_maxpool
+
+class LSTMModel(nn.Module):
+
+    def __init__(self, input_size, hidden_size, num_layers=1):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
+                            num_layers=num_layers, batch_first=True)
+
+    def forward(self, x):
+        x = x.squeeze(1)
+        x, _ = self.lstm(x)
+        return x.unsqueeze(1)
 
 
 class TextCNNModel(nn.Module):
 
-    def __init__(self, embedding_matrix):
+    def __init__(self):
         super(TextCNNModel, self).__init__()
-        self.embedding_dim = config['embedding_dim']
-        self.embedding_matrix = embedding_matrix
-
         # 特征提取层
         self.b0 = Block(5)
         self.b1 = Block(7)
@@ -48,13 +58,12 @@ class TextCNNModel(nn.Module):
 
         # 特征融合部分
         self.neck = nn.Sequential(
-            nn.Linear(2 * 6 * config['hidden_layer'], 512),
+            nn.Linear(6 * config['hidden_layer'], 512),
             nn.ReLU(),
             nn.Linear(512, config['nc'])
         )
 
     def forward(self, x, label=None):
-        x = self.embedding_matrix(x)
 
         b0_results = self.b0(x)
         b1_results = self.b1(x)
@@ -68,3 +77,21 @@ class TextCNNModel(nn.Module):
         output = self.neck(features)
         return output
 
+
+class LSTM_TextCNN(nn.Module):
+    def __init__(self, embedding_matrix):
+        super(LSTM_TextCNN, self).__init__()
+        self.embedding_dim = config['embedding_dim']
+        self.embedding_matrix = embedding_matrix
+
+        # 长短记忆神经网络先处理位置关系
+        self.lstm = LSTMModel(config['embedding_dim'], config['lstm_hidden_layer'])
+        # 通过CNN再提取特征
+        self.textCNN = TextCNNModel()
+
+
+    def forward(self, x):
+        x = self.embedding_matrix(x)
+        lstm_out = self.lstm(x)
+        cnn_out = self.textCNN(lstm_out)
+        return cnn_out
